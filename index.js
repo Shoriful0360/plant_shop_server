5cb9548303339feb4aeed5403d4553c8ac8994b1
Shoriful <shorifulbba0360@gmail.com>
2025-01-12 20:38:35 +0600

    added manage order page functionality

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use(morgan('dev'))
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token
-
+console.log('from verify tokes',token)
   if (!token) {
     return res.status(401).send({ message: 'unauthorized access' })
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err)
+
       return res.status(401).send({ message: 'unauthorized access' })
     }
     req.user = decoded
@@ -55,7 +55,29 @@ async function run() {
     const userCollection=client.db('plant_shop').collection('users')
     const orderCollection=client.db('plant_shop').collection('order')
 
+// verify Admin
+const verifyAdmin=async(req,res,next)=>{
+  const email=req.user?.email;
+  
+  const query={email}
+  const result=await userCollection.findOne(query)
+  if(!result || result.role !=='Admin'){
+    return res.status(403).send('cannot access without admin')
+  }
+  next()
+}
 
+// verify seller
+const verifySeller=async(req,res,next)=>{
+  const email=req.user?.email;
+  
+  const query={email}
+  const result=await userCollection.findOne(query)
+  if(!result || result.role !=='Seller'){
+    return res.status(403).send('cannot access without seller')
+  }
+  next()
+}
 
     // Generate jwt token
     app.post('/jwt', async (req, res) => {
@@ -98,14 +120,14 @@ async function run() {
       }
       const result=await userCollection.insertOne({
         ...user,
-        role:'customer',
+        role:'Customer',
        timeStamp:Date.now()
       })
       res.send(result)
     })
 
     // plants collection
-    app.post('/plant',async(req,res)=>{
+    app.post('/plant',verifyToken,verifySeller,async(req,res)=>{
       const plants=req.body;
       const result=await plantsCollection.insertOne(plants,{role:'customer'})
       res.send(result)
@@ -117,6 +139,23 @@ async function run() {
       res.send(result)
     })
 
+    // get plant specific user
+    app.get('/plants/:email',async(req,res)=>{
+      const email=req.params.email;
+      const query={'seller.email':email}
+      const result=await plantsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // delet plant specific user by id
+
+    app.delete('/plant/:id',verifyToken,verifySeller,async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result=await plantsCollection.deleteOne(query)
+
+      res.send(result)
+    })
     // get one plant by id
     app.get('/plant/:id',async(req,res)=>{
       const id=req.params.id;
@@ -141,10 +180,100 @@ app.patch('/users/:email',async(req,res)=>{
   res.send(result)
 })
 
+// get all users admin route
+app.get('/all_user/:email',verifyToken,verifyAdmin,async(req,res)=>{
+const email=req.params.email;
+const query={email:{$ne:email}}
+const result=await userCollection.find(query).toArray()
+res.send(result)
+})
+
+
+// user role
+app.get('/users/role/:email',async(req,res)=>{
+  const email=req.params.email;
+  const query={email};
+
+  const result=await userCollection.findOne(query)
+  res.send({role:result?.role})
+})
+
+// update user role
+app.patch('/users/role/:email',verifyToken,async(req,res)=>{
+  const email=req.params.email;
+  const filter={email}
+  const {role}=req.body;
+  const updateDoc={
+    $set:{role,status:'Verified'}
+  }
+  const result=await userCollection.updateOne(filter,updateDoc)
+  res.send(result)
+})
+
     // order related api
+    // seller
+app.get('/seller_order/:email',async(req,res)=>{
+  const email=req.params.email;
+  const query={seller:email}
+  console.log('from seler_order',query)
+  const result=await orderCollection.aggregate([
+    {
+      $match:query, //Match specefic customers data only by eamil
+    },
+    {
+      $addFields:{
+        PlantId:{$toObjectId:'$PlantId'} //conver plant id string field to objectId field
+      }
+    },
+    {
+      $lookup:{  //go to difference collection and look for data
+        from:'plants', 
+        localField:'PlantId', //local data that you want to match 
+        foreignField:'_id', //Foreign field name of that same data
+        as:'plantInfo', //return the data as plants array
+      }
+    },
+
+    // convert object array of data &unwind   
+    {
+      $unwind:'$plantInfo'
+    },
+
+    // some data remove in plant info as for that  add filed
+    {
+      $addFields:{
+        ImageName:'$plantInfo.name',
+        Image:'$plantInfo.imgUrl',
+        category:'$plantInfo.category',
+      }
+    },
+    // specefic get data from plant info then remove plant info note: if 1 then give this data and remove another data, if 0 then remove this data and give another data
+    {
+      $project:{
+        plantInfo:0,
+      }
+    }
+   
+  ])
+  .toArray()
+  res.send(result)
+})
+
+// order status changing 
+app.patch('/seller_order/:id',verifyToken,verifySeller,async(req,res)=>{
+  const id=req.params.id;
+  const {status}=req.body;
+  const query={_id:new ObjectId(id)}
+  const updateDoc={
+    $set:{status}
+  }
+  const result=await orderCollection.updateOne(query,updateDoc)
+ res.send(result)
+})
+
     app.post('/order',verifyToken,async(req,res)=>{
       const orderInfo=req.body;
-      console.log(orderInfo)
+
       const result=await orderCollection.insertOne(orderInfo)
       res.send(result)
     })
